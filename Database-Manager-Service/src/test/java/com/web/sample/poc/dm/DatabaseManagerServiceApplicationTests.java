11dac8c5bc8f1642cb48f377a7b34405d944a68f
Dinoshna Thathuru <dinulearnings@gmail.com>
2024-05-10 02:52:42 +0530

    dependencies added for lombok and slf4j

--- Database-Manager-Service/src/test/java/com/web/sample/poc/dm/DatabaseManagerServiceApplicationTests.java
+++ Database-Manager-Service/src/test/java/com/web/sample/poc/dm/DatabaseManagerServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.web.sample.poc.DatabaseManagerService;
+package com.web.sample.poc.dm;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
